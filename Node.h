965e50ab0c00c8ca824110877e352bf628fa1e88
yagmruodtu <90095338+yagmruodtu@users.noreply.github.com>
2023-02-21 09:29:04 +0200

    Update Node.h

--- Node.h
+++ Node.h
@@ -1,4 +1,13 @@
+/* Creator : Yagmur Saygili / School Assigment
 
+ 	Self created library file to use modular approach
+	and make it easier to read or inspect the code later on.
+	
+	Here Node (Like a node in Tree) consepts, functions and their declarations 
+	are defined.
+
+    Node structure is defined here as well.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
